459ecb2024dbc0ecb98e45837b03fd20b90f41b2
Emilio Bensignor <liobensignor@gmail.com>
2024-11-16 19:01:07 -0300

    Index Maquetacion + Responsive 1080px

--- web_app/main.js
+++ web_app/main.js
@@ -2,13 +2,22 @@
 const introduccion = document.getElementById("introduccion");
 const seccionManager = document.getElementById("seccionManager");
 const seccionEquipo = document.getElementById("seccionEquipo");
+const pasoUno = document.getElementById("pasoUno");
+const pasoDos = document.getElementById("pasoDos");
 
 
 // Boton
 const btnEmpezar = document.getElementById("btnEmpezar");
 btnEmpezar.addEventListener("click", empezar);
+
 const btnManager = document.getElementById("btnManager");
 btnManager.addEventListener("click", crearManager);
+
+const confirmarFormacion = document.getElementById("confirmarFormacion");
+confirmarFormacion.addEventListener("click", pasoDosFormacion);
+
+const btnEquipo = document.getElementById("btnEquipo");
+btnEquipo.addEventListener("click", terminarEquipo);
 
 
 // Funciones
@@ -21,4 +30,15 @@ function crearManager(event) {
   event.preventDefault();
   seccionManager.style.display = "none";
   seccionEquipo.style.display = "flex";
+}
+
+function pasoDosFormacion() {
+  pasoUno.style.display = "none";
+  pasoDos.style.display = "flex";
+}
+
+function terminarEquipo(event) {
+  event.preventDefault();
+  seccionEquipo.style.display = "none";
+  window.location.href = "/web_app/misEquipos.html";
 }
